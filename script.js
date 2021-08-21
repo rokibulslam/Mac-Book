331dd681a415881dbf92e9a9cdebcef7ea33fd71
rokibulislamm <md>
2021-08-21 19:51:37 +0600

    some revision and add function for discount offer

--- script.js
+++ script.js
@@ -16,11 +16,26 @@ function calculateTotal(){
     const bestPrice = getPerProductPrice('best-price')
     const memoryPrice = getPerProductPrice('memory-price')
     const storagePrice = getPerProductPrice('storage-price')
-    const shippingPrice = getPerProductPrice('delivery-price')
+    const shippingPrice = getPerProductPrice('delivery-charge')
     const totalAmount = bestPrice + memoryPrice + storagePrice + shippingPrice;
     document.getElementById('total-price').innerText = totalAmount;
     document.getElementById('total-promo-price').innerText = totalAmount;
     return totalAmount;
+}
+
+// Discount calculation
+function discountTotal(){
+    const promoCodeInput = document.getElementById('promo-code-apply');
+    const promoCode = promoCodeInput.value;
+    // add promoCode and discount calculation
+    if (promoCode == 'stevekaku'){
+        // 20% discount on total price
+        const discount = (calculateTotal() / 100) * 20;
+        const grandTotalAmountText = document.getElementById('total-promo-price');
+        const grandTotalAmount = parseInt(grandTotalAmountText.innerText) ;
+        const promoAmount = grandTotalAmount - discount;
+        grandTotalAmountText.innerText = promoAmount;
+    }
 }
 
 // memory cost 
@@ -50,26 +65,14 @@ document.getElementById('btn-1tb-ssd').addEventListener('click', function(){
 // shipping Cost 
 // regular price 
 document.getElementById('btn-shipping-regular').addEventListener('click', function(){
-    upDateOnCart('delivery-price', 0);
+    upDateOnCart('delivery-charge', 0);
 });
 // fast delivery price 
 document.getElementById('btn-shipping-fast').addEventListener('click', function(){
-    upDateOnCart('delivery-price', 20);
+    upDateOnCart('delivery-charge', 20);
 });
-
     
 // Promo code apply and discount 20% 
 document.getElementById('btn-promo-code').addEventListener('click', function(){
-    const promoText = document.getElementById('promo-code-apply');
-    console.log(promoText.value)
-    const promoCode = promoText.value;
-    // add promoCode and discount calculation
-    if (promoCode == 'stevekaku'){
-        // 20% discount on total price
-        const bonus = calculateTotal() / 20;
-        const grandTotalAmountText = document.getElementById('total-promo-price');
-        const grandTotalAmount = parseInt(grandTotalAmountText.innerText) ;
-        const promoAmount = grandTotalAmount - bonus;
-        grandTotalAmountText.innerText = promoAmount;
-    }
+   discountTotal();
 })
